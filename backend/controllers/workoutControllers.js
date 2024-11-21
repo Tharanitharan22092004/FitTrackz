3940a36f3415b353bea2b1adc2dfcde9fc13e86c
Tharanitharan22092004 <tharanitharan.n2022cse@sece.ac.in>
2024-11-21 18:30:50 +0530

    protecting API routes

--- backend/controllers/workoutControllers.js
+++ backend/controllers/workoutControllers.js
@@ -1,13 +1,11 @@
 const Workout = require("../models/workoutModel");
 const mongoose = require("mongoose");
 
-
 const getWorkouts = async (req, res) => {
   const workouts = await Workout.find({}).sort({ createdAt: -1 });
 
   res.status(200).json(workouts);
 };
-
 
 const getWorkout = async (req, res) => {
   const { id } = req.params;
@@ -24,7 +22,6 @@ const getWorkout = async (req, res) => {
 
   res.status(200).json(workout);
 };
-
 
 const createWorkout = async (req, res) => {
   const { title, load, reps } = req.body;
@@ -46,7 +43,6 @@ const createWorkout = async (req, res) => {
       .json({ error: "Please fill in all fields", emptyFields });
   }
 
-
   try {
     const workout = await Workout.create({ title, load, reps });
     res.status(200).json(workout);
@@ -54,7 +50,6 @@ const createWorkout = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 };
-
 
 const deleteWorkout = async (req, res) => {
   const { id } = req.params;
@@ -71,7 +66,6 @@ const deleteWorkout = async (req, res) => {
 
   res.status(200).json(workout);
 };
-
 
 const updateWorkout = async (req, res) => {
   const { id } = req.params;
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,26 +1,24 @@
 const express = require("express");
 const {
+  createWorkout,
   getWorkouts,
   getWorkout,
-  createWorkout,
   deleteWorkout,
   updateWorkout,
 } = require("../controllers/workoutControllers");
+const requireAuth = require("../middleware/requireAuth");
 
 const router = express.Router();
 
+router.use(requireAuth);
 
 router.get("/", getWorkouts);
 
-
 router.get("/:id", getWorkout);
-
 
 router.post("/", createWorkout);
 
-
 router.delete("/:id", deleteWorkout);
-
 
 router.patch("/:id", updateWorkout);
 
